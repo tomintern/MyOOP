20f8c2cd31a469981e51f705bac6a23e89e559b7
tomintern <internyp@gmail.com>
2016-07-28 14:15:21 +0700

    initial commit

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -10,16 +10,21 @@ public class Calculator {
     public static void main(String[] args) {
           Circle c = new Circle();
         
-        float t,t1;
+        float t,t1,t2,t3;
         Scanner sn = new Scanner(System.in);
-        System.out.print("Input = ");
+        System.out.print("Input radius = ");
         t=sn.nextFloat();
-        System.out.print("Input = ");
+        System.out.print("Input radius = ");
         t1=sn.nextFloat();
         System.out.print("Result of Circle = "+c.setradius(t, t1));
+        System.out.println("----------------------------------------");
       
         
-       
+        System.out.print("Input triangle = ");
+        t2=sn.nextFloat();
+        System.out.print("Input triangle = ");
+        t3=sn.nextFloat();
+        System.out.print("Result of triangle = "+c.caltriangle(t2, t3));
         
     }
     
--- src/calculator/Circle.java
+++ src/calculator/Circle.java
@@ -6,19 +6,26 @@ public class Circle {
     public static float r;
     public static float r1;
     public static float pi = (float) 3.14;
+    public static float triangle = (float) 0.5;
     public static float  sum;
     public Circle(){
+        
         
     }
     public float setradius(float r ,float r1){
         this.r=r;
         this.r1=r1;
         this.sum=sum;
-  
         sum = pi*(r*r1);
-    
         return sum;
         
+    }
+    public float caltriangle(float t,float t1){
+        this.sum = sum;
+        this.r=t;
+        this.r1=t1;
+        sum= triangle*(t*t1);
+        return sum;
     }
     
 }
